9b5afdb7fa416f531f7376bb9adb701a0c998ae9
gomahthy <gomahthy.ganesan@novuna.co.uk>
2023-07-02 20:19:51 +0100

    Jira 1

---
+++ src/main/java/uk/axone/devintest/git.java
@@ -0,0 +1,5 @@
+package uk.axone.devintest;
+
+public class git {
+
+}
---
+++ src/test/java/uk/axone/devintest/gittest.java
@@ -0,0 +1,11 @@
+package uk.axone.devintest;
+
+
+import org.junit.Test;
+
+public class gittest {
+    @Test
+    public void testpack1(){
+
+    }
+}
